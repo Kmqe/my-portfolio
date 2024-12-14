f857457d1c8cd95aa26ab29c762c4f2303bf7a1d
Kmqe <7moodi115b2@gmail.com>
2024-12-14 16:36:48 +0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 let skillPers = document.querySelectorAll(".skill-per");
-console.log(skillPers)
+const spinner_overlay = document.getElementById("spinner_overlay");
+const textArea = document.querySelector("textarea").value = "";
+const toUp = document.getElementById("to_up");
+
+window.addEventListener("scroll", () => {
+    if(scrollY > 150){
+        toUp.classList.add("show")
+    }
+    else {
+        toUp.classList.remove("show")
+    }
+})
+
+window.addEventListener("load" , () => {
+    setTimeout(() => {
+        spinner_overlay.classList.add("remove")
+        spinner_overlay.style.opacity = 0;
+    } , 300)
+})
 
 skillPers.forEach(skill => {
     skill.style.width = skill.dataset.skill + "%";
 })
 
-const textArea = document.querySelector("textarea").value = "";
-textArea()
